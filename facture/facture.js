548436ab802b87987d728d5295d2fa293c3fba2a
Charisse <145470757+CharisseHuerto@users.noreply.github.com>
2023-10-25 18:39:45 +0200

    Add files via upload

--- facture/facture.js
+++ facture/facture.js
@@ -1,26 +1,28 @@
-function Facture() {
-    var a =document.getElementById("D").value;
-    var b =document.getElementById("E").value;
+function facture()
+{
+    var a =document.getElementById("A").value;
+    var b =document.getElementById("B").value;
     var c = Number(a)*Number(b);
-    t3.value=(c)
+    r1.value=(c)
  
  
-    var d =document.getElementById("G").value;
-    var e =document.getElementById("H").value;
+    var d =document.getElementById("C").value;
+    var e =document.getElementById("D").value;
     var f = Number(d)*Number(e);
-    t45.value=(f)
+    r2.value=(f)
  
  
-    var h =document.getElementById("J").value;
-    var j =document.getElementById("K").value;
-    var k = Number(h)*Number(j);
-    L.value=(k)
+    var g =document.getElementById("E").value;
+    var h =document.getElementById("F").value;
+    var i = Number(g)*Number(h);
+    r3.value=(i)
  
  
-    var somme = Number(k)+Number(c)+Number(f);
-    t5.value=(somme)
+    var j = Number(c)+Number(f)+Number(i);
+    r4.value=(j)
+}
  
- 
- }
- 
+function reinitialiser(){
+    window.location.reload();
+}
  
--- moyenne/moyenne.js
+++ moyenne/moyenne.js
@@ -7,20 +7,25 @@ function moyenne()
     var coef2 = document.getElementById("c2").value;
     var coef3 = document.getElementById("c3").value;
     var s = Number(coef1)+Number(coef2)+Number(coef3);
-    var m = Number(note1)*Number(coef1)+Number(note2)*Number(coef2)+Number(note3)*Number(coef3) /s;
+    var m = [Number(note1)*Number(coef1) + Number(note2)*Number(coef2) + Number(note3)*Number(coef3)]/s;
 
     moy.value = m;
 
     if (m < 10)
         resultat.value = "Redoublant";
+
     else if (m < 12)
         resultat.value = "Admis passable";
+
     else if (m < 14)
         resultat.value = "Admis assez bien";
+
     else if (m < 16)
         resultat.value = "Admis bien";
+
     else if (m <= 20)
         resultat.value = "Admis très bien";
+    
     else if (m > 20)
         resultat.value = "Erreur";
 
